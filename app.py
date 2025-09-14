2bcb13471e69b35f8a4613b3656aee38fa430cae
7aidproductionsweb <7aiproductions@gmail.com>
2025-09-14 15:18:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-from fastapi import FastAPI
-import spacy
-
-app = FastAPI()
-nlp = spacy.load("fr_core_news_md")
-
-@app.get("/lemmatize")
-def lemmatize(word: str):
-    doc = nlp(word)
-    lemmas = [token.lemma_ for token in doc if token.pos_ == "VERB"]
-    return {"input": word, "lemma": lemmas[0] if lemmas else None}
+nlp = spacy.load("fr_core_news_sm")
